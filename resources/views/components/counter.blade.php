fc256ab155ad136a06e605076a56490d102462a9
Adrian Nürnberger <adrian@nuernberger.me>
2023-04-15 10:12:49 +0200

    extract alpine component

--- resources/views/components/counter.blade.php
+++ resources/views/components/counter.blade.php
@@ -1,30 +1,6 @@
 @props(['counter' => 0])
 
-<div x-data="{
-        counter: @js($counter),
-        init() {
-            this.showNote()
-            this.$watch('counter', () => this.showNote())
-        },
-        increment() {
-            this.counter++
-
-            if(this.counter > 9) this.$refs.note.style.display = 'block'
-        },
-        decrement() {
-            this.counter--
-        },
-        isCounterTooBig() {
-            return this.counter > 2
-        },
-        showNote() {
-            if(this.isCounterTooBig()) {
-                this.$refs.note.style.display = 'block'
-            } else {
-                this.$refs.note.style.display = 'none'
-            }
-        }
-    }">
+<div x-data="counter(@js($counter))">
     <button @click="decrement" type="button">-</button>
     <span x-text="counter"></span>
     <button @click="increment" type="button">+</button>
